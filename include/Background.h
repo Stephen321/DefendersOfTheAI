816adc7fd777c7fa83d66946c050c14673f78ba3
OneEyedBanshee <craig_johnsto@msn.com>
2017-01-12 23:03:11 +0000

    added basic shape generation for surface, seemingly broke other background stuff??

--- include/Background.h
+++ include/Background.h
@@ -8,9 +8,11 @@ public:
 	Background(const sf::Vector2f& screenSize);
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	void update(float worldVelX);
+	void generateSurface(const sf::Vector2f& screenSize);
 	sf::Vector2u getWorldSize() const;
 
 private:
 	const int LAYERS = 1;
 	std::vector<Layer> m_layers;
+	std::vector<sf::ConvexShape> m_surfaceShapes;
 };
--- src/Background.cpp
+++ src/Background.cpp
@@ -3,7 +3,71 @@
 Background::Background(const sf::Vector2f& screenSize)
 {
 	m_layers.push_back(Layer("assets/sprites/enviroment/stars", screenSize, 3, 0.33f));
-	m_layers.push_back(Layer("assets/sprites/enviroment/surface", screenSize, 9, 1.f));
+	//m_layers.push_back(Layer("assets/sprites/enviroment/surface", screenSize, 9, 1.f));
+
+	generateSurface(screenSize);
+}
+
+void Background::generateSurface(const sf::Vector2f& screenSize)
+{
+	int xPos = 0;
+	int yPos = screenSize.y * 0.75f;
+	int worldWidth = screenSize.x * 9;
+	int currentWidth = 0;
+
+	while (currentWidth < worldWidth)
+	{
+		sf::ConvexShape convex;
+
+		int width = rand() % 160 + 40;
+		int height = 0;
+
+		if (m_surfaceShapes.size() % 2 == 0)
+		{
+			height += rand() % 2 + 1;
+
+			if (rand() % 2 == 0 || yPos > screenSize.y * 0.9)
+			{
+				height *= -1;
+			}
+
+			height *= 10;
+			width = abs(height);
+		}
+
+		if (currentWidth + width > worldWidth)
+		{
+			width -= (currentWidth + width) - worldWidth;
+		}
+
+		// resize it to 4 points
+		convex.setPointCount(4);
+
+		// define the points
+		convex.setPoint(0, sf::Vector2f(xPos, yPos));
+		convex.setPoint(1, sf::Vector2f(xPos + width, yPos + height));
+		convex.setPoint(2, sf::Vector2f(xPos + width, screenSize.y));
+		convex.setPoint(3, sf::Vector2f(xPos, screenSize.y));
+		
+		if (convex.getPoint(0).y == convex.getPoint(1).y) //flat surface, use default colour
+		{
+			convex.setFillColor(sf::Color(196, 55, 98));
+		}
+		else if (convex.getPoint(0).y < convex.getPoint(1).y) //decline, use shade colour
+		{
+			convex.setFillColor(sf::Color(119, 24, 52));
+		}
+		else //incline, use highlight colour
+		{			
+			convex.setFillColor(sf::Color(222, 112, 145));
+		}				
+
+		m_surfaceShapes.push_back(convex);
+		
+		currentWidth += width;
+		xPos = currentWidth;
+		yPos += height;
+	}
 }
 
 void Background::draw(sf::RenderTarget & target, sf::RenderStates states) const
@@ -12,6 +76,11 @@ void Background::draw(sf::RenderTarget & target, sf::RenderStates states) const
 	{
 		target.draw(layer);
 	}
+
+	for (int i = 0; i < m_surfaceShapes.size(); i++)
+	{
+		target.draw(m_surfaceShapes[i]);
+	}
 }
 
 void Background::update(float worldVelX)
@@ -19,6 +88,11 @@ void Background::update(float worldVelX)
 	for (Layer& layer : m_layers)
 	{
 		layer.update(worldVelX);
+	}
+
+	for (int i = 0; i < m_surfaceShapes.size(); i++)
+	{
+		m_surfaceShapes[i].move(sf::Vector2f(worldVelX, 0));
 	}
 }
 
